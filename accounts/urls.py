d6d328099ff50a89848fd72305eb22c00e865495
sureeyanee <6310510026@student.tu.ac.th>
2025-05-31 04:30:40 +0700

    complete role

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import register_user, login_user, logout_user, role_redirect, plant_owner_dashboard_view, data_analyst_dashboard_view, drone_controller_dashboard_view, plant_detail_analyst_view
-
+from .views import register_user, login_user, logout_user, role_redirect, plant_owner_dashboard_view, data_analyst_dashboard_view, drone_controller_dashboard_view, plant_detail_analyst_view, complete_profile
 
 urlpatterns = [
     path('signup/', register_user, name='register_page'),
@@ -11,4 +10,5 @@ urlpatterns = [
     path('dashboard/data-analyst/', data_analyst_dashboard_view, name='data_analyst_dashboard'),
     path('dashboard/drone-controller/', drone_controller_dashboard_view, name='drone_controller_dashboard'),
     path('dashboard/data-analyst/plant/<int:plant_id>/', plant_detail_analyst_view, name='plant_detail_analyst'),
+    path('complete-profile/', complete_profile, name='complete_profile'),
 ]
